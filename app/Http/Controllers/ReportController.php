3de5aede294e872840e7e585b0d442264bc1c1b7
Abdelrahman404 <abdelrahman01018006909@gmail.com>
2023-04-27 13:42:52 +0200

    seller report adjustment

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -30,7 +30,8 @@ class ReportController extends BaseController
 
         $validator =  Validator::make($request->all(), [
             'seller_id' => 'required|exists:users,id',
-            'duration' => 'required|integer|between:1,7'
+            'from' => 'required|string',
+            'to' => 'required|string'
         ]);
 
         if ($validator->fails()){
@@ -38,10 +39,93 @@ class ReportController extends BaseController
             return $this->sendError($validator->errors());
         }
 
+        $from = Carbon::parse($request->from);
+        
+        $to = Carbon::parse($request->to);
+
         $user = User::findOrFail($request->seller_id)->makeVisible(['name_en', 'name_ar']);
 
+        $output = $this->fromToDurationReport($user, $from, $to);
 
+        return $this->sendResponse($output);
+    }
+
+    public function fromToDurationReport($user, $startDate, $endDate){
+
+        $output = [];
         
+        $user->report_duration = $startDate . '-' . $endDate;
+
+        $user->makeHidden(['userInfo', 'name_en', 'name_ar', 'type',]);
+
+        $output['user'] = $user;
+
+        $output['calls'] = Call::where('created_by', $user->name_en)
+                            ->orWhere('created_by', $user->name_ar)
+                            ->whereBetween('created_at', [$startDate, $endDate])
+                            ->count();
+ 
+        $output['registered_clients'] = Client::where('created_by', $user->name_en)
+                                        ->orWhere('created_by', $user->name_ar)
+                                        ->whereBetween('created_at', [$startDate, $endDate])
+                                        ->count();
+        
+        // Getting user deals at this period
+        $deals = Deal::whereBetween('created_at', [$startDate, $endDate])->get();
+
+        foreach($user->userInfo as $userInfo){
+
+            // Seller information
+            $collection = [];
+
+            $collection['country'] = $userInfo->country->name;
+            $collection['target'] = $userInfo->target;
+            $collection['comission'] = $userInfo->comission;
+            $collection['achived'] = 0;
+    
+            // Seller deals done (ربح الثفقة)
+            foreach($deals as $deal){
+
+                $achived = 0;
+                // checking if deal done at the same country
+                if($deal->country_id == $userInfo->country_id){
+
+                    $achived = $achived + $deal->amount;
+
+                    $collection['achived'] = $collection['achived'] + $achived;
+
+                }
+            }
+        
+        $collection['left'] = $userInfo->target - $collection['achived'];
+        $collection['comission_value'] = $userInfo->comission / 100 * $collection['achived'];
+    
+       
+                $records = collect([]);
+                $records->push(collect($collection));
+            
+        }
+        $output['records'] = $records;
+
+        $cases = Status::all();     
+
+        $casesCollection = collect();
+
+        foreach($cases as $case){
+            
+            $count = Client::whereBetween('created_at', [$startDate, $endDate])
+                            ->where('created_by', $user->name_en)
+                            ->orWhere('created_by', $user->name_ar)
+                            ->where('status', $case->id)
+                            ->count();
+
+            $casesCollection->push(collect(['id' => $case->id, 'name' => $case->name, 'count' => $count] ));
+            
+        }
+        
+        $output['cases'] = $casesCollection;
+        
+        return $output;
 
     }
     public function oldSellerReport(Request $request){
@@ -75,7 +159,7 @@ class ReportController extends BaseController
         return $this->sendResponse($output);
 
     }
-
+    
     public function todayReport($user){
 
         $output = [];
