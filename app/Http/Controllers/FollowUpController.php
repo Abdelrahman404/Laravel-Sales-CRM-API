3fc498944ae70c30cb125b5c1706cda38679b3fb
Abdelrahman404 <abdelrahman01018006909@gmail.com>
2023-04-27 10:53:54 +0200

    new filter adjustments

--- app/Http/Controllers/FollowUpController.php
+++ app/Http/Controllers/FollowUpController.php
@@ -7,6 +7,7 @@ use App\Models\PossibilityOfReply;
 use App\Models\Status;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class FollowUpController extends BaseController
 {
@@ -17,12 +18,24 @@ class FollowUpController extends BaseController
 
     public $status = 1;
 
+    public $from;
+
+    public $to;
+
+    public $call_response_type_id;
+
+    public $seller_id;
+
     public function index(Request $request){
     
     // Update values based on request
     if ($request->filled('rows')) { $this->rows = $request->input('rows');}
     if ($request->filled('word')) {$this->word = $request->input('word');}
     if ($request->filled('status')) {$this->status = $request->input('status');}
+    if ($request->filled('from')) {$this->from = $request->input('from');}
+    if ($request->filled('to')) {$this->to = $request->input('to');}
+    if ($request->filled('call_response_type_id')) {$this->call_response_type_id = $request->input('call_response_type_id');}
+    if ($request->filled('seller_id')) {$this->seller_id = $request->input('seller_id');}
     
         $clients = Client::where('active', true)
                         ->where('status', $this->status)
@@ -31,11 +44,34 @@ class FollowUpController extends BaseController
                         ->withSum('deals', 'amount');
             
         $clients = $request->word ? $clients->where('name', 'LIKE', "%{$request->word}%"): $clients;
+
+        $clients = $request->from ? $clients->where('created_at', '>=', Carbon::parse($request->from)): $clients;
+
+        $clients = $request->to ? $clients->where('created_at', '<=', Carbon::parse($request->to)): $clients;
+
+        $clients = ($request->from && $request->to) ? $clients->whereBetween('created_at', [Carbon::parse($request->from), Carbon::parse($request->to)]) : $clients;
     
+        // Filter by seller and result will by clients added by this seller
+       if($request->filled('seller_id')){
+
+            $user = User::find($request->seller_id);
+
+            $clients = $clients->where('created_by', $user->name_ar)->orWhere('created_by', $user->name_ar);
+       }
+   
         $clients = $clients->latest()->paginate($this->rows);
+
+        // Adding last call status for each client حالة الرد
+        $clients->getCollection()->map(function ($client) {
+
+            $client['last_call_status'] = $client->calls->last()->possibilityOfReply->name;
+
+              return $client;
+        });
 
         $cases = Status::all();
 
+        // Client cases collection (Tabs)
         $casesCollection = collect();
 
         foreach($cases as $case){
